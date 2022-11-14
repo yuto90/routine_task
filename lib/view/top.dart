380b508c6a37c9d88acc75968ec2f2cb19dfab8b
yuto90 <solidusgako4087@gmail.com>
2022-11-14 20:12:00 +0900

    GridViewでGitHubの草生やすやつ作成

--- lib/view/top.dart
+++ lib/view/top.dart
@@ -26,12 +26,47 @@ class Top extends ConsumerWidget {
         shrinkWrap: true,
         itemCount: ref.read(routineProvider.notifier).state,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: const Icon(Icons.face),
-            title: Text('$index'),
-            subtitle: const Text('ルーティン'),
-            trailing: const Icon(Icons.more_vert),
-          );
+          return Column(
+            children: [
+              ListTile(
+                  leading: const Icon(Icons.face),
+                  title: Text('$index'),
+                  subtitle: const Text('ルーティン'),
+                  trailing: Text('$index日継続中')),
+              // todo ルーティン毎に草生やせるようにする
+              Container(
+                //color: Colors.red,
+                height: SizeConfig.blockSizeVertical! * 15,
+                width: double.infinity,
+                child: GridView.count(
+                  // GridViewをスクロールさせない
+                  physics: const NeverScrollableScrollPhysics(),
+                  // 横へWidgetを出す
+                  scrollDirection: Axis.horizontal,
+                  // 1行あたりに表示するWidgetの数
+                  crossAxisCount: 7,
+                  // Widget間のスペース（上下）
+                  mainAxisSpacing: 4,
+                  // Widget間のスペース（左右）
+                  crossAxisSpacing: 4,
+                  // 全体の余白
+                  padding: const EdgeInsets.all(4),
+                  children: [
+                    for (int i = 0; i < 140; i++) ...[
+                      Container(
+                        decoration: BoxDecoration(
+                          color: ref.read(contributeMock.notifier).state[i] == 1
+                              ? Colors.green[400]
+                              : Colors.grey[300],
+                          borderRadius: BorderRadius.circular(2),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+              )
+            ],
+          ); // todo
         },
       ),
       floatingActionButton: FloatingActionButton(
